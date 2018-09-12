f6d401e7b4a817110574ddaff835b1d446d69a98
Boian <boian.filev@gmail.com>
2018-09-12 09:05:19 -0400

    Renamed main file

--- gominion.go
+++
@@ -1,4 +0,0 @@
-package gominion
-
-func Play() {
-}
---
+++ main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+    "fmt"
+)
+
+func Play() {
+    fmt.Println("Time to Play the Game")
+}
+
+func main() {
+    Play()
+}
